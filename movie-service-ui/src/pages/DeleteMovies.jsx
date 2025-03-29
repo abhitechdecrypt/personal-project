19560dbfec2a63a8fe58fb5be37fceb614b7483e
Abhishek Kumar <abhithakur291998@gmail.com>
2025-03-29 19:13:43 +0530

    Removing the forntend code from backed branch

--- movie-service-ui/src/pages/DeleteMovies.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DeleteMovies() {
-  return (
-    <div>DeleteMovies</div>
-  )
-}
--- movie-service-ui/src/pages/EditMovies.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function EditMovies() {
-  return (
-    <div>EditMovies</div>
-  )
-}
